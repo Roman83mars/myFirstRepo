71c51308da123a63e6a3c3065ae2e29a2cc287b7
Roman Amb <roman83mars@gmail.com>
2024-02-23 05:22:44 +0300

    первый коммит задания 1

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-=======
 const a = "Переменная из lesson03"
->>>>>>> lesson03
 const b = "Наша ветка lesson01"
 
 const logged = () => {
     console.log('Наконец мы узнали все команды, необходимые для работы с GIT')
 }
+
+alert('HW')
