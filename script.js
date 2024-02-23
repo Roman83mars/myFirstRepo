2df73709d16db01c5b120da92e9ac4e8d36dbc50
Roman Amb <roman83mars@gmail.com>
2024-02-24 01:29:46 +0300

    Задание второго урока

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const a = 1
-const b = "Наша ветка lesson01"
+let title = 'Second project'
+let screens = 'Простые, Сложные, Интерактивные'
+let screenPrice = 5000
+let rollback = 30
+let fullPrice = 120000
+let adaptive = true
 
-const logged = () => {
-    console.log('Наконец мы узнали все команды, необходимые для работы с GIT')
-}
+console.log(typeof title)
+console.log(typeof fullPrice)
+console.log(typeof adaptive)
+console.log(typeof title, typeof fullPrice, typeof adaptive)
+
+console.log(screens.length);
+
+let screenPriceProposal = 'Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани'
+
+console.log(screenPriceProposal);
+
+let fullPriceProposal = 'Стоимость разработки сайта ' + fullPrice + ' рублей/долларов/гривен/юани'
+
+console.log(fullPriceProposal);
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log(fullPrice * rollback / 100);
